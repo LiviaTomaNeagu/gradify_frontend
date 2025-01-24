bf4f5c65c0a1eb05cb815ad4232fb6e7df3bfb79
LiviaTomaNeagu <livia.toma@student.unitbv.ro>
2025-01-24 15:55:14 +0200

    [Fix] Added occupation name and date formatting

--- package/src/app/features/forum/core/interfaces/get-questions.dto.ts
+++ package/src/app/features/forum/core/interfaces/get-questions.dto.ts
@@ -15,6 +15,7 @@ export interface GetQuestionsResponseDTO {
     topic: Topic;
     name: string;
     surname: string;
+    occupationName: string;
     answers: GetAnswerResponseDTO[];
   }  
   
--- package/src/app/pages/forum/forum-card/forum-card.component.ts
+++ package/src/app/pages/forum/forum-card/forum-card.component.ts
@@ -4,21 +4,27 @@ import { MatCard } from '@angular/material/card';
 import { JsonPipe } from '@angular/common';
 import { GetQuestionResponseDTO } from 'src/app/features/forum/core/interfaces/get-questions.dto';
 import { getTopicName, Topic } from 'src/app/shared/enums/topic.enum';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-forum-card',
   standalone: true,
   imports: [MaterialModule, MatCard, JsonPipe],
+  providers: [DatePipe],
   templateUrl: './forum-card.component.html',
   styleUrls: ['./forum-card.component.scss'],
   
 })
 export class ForumCardComponent {
   topicsList: string[] = [];
+  formattedDate: string = '';
+  constructor(private datePipe: DatePipe) {}
+
 
   ngOnInit(): void {
     if (this.question?.topic) {
       this.topicsList = Array(5).fill(getTopicName(this.question.topic));
+      this.formattedDate = this.datePipe.transform(this.question.createdAt, 'dd-MMMM-yyyy') || '';
     } else {
       console.error('Question topic is not defined or invalid');
     }
