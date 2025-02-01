552e7aba23fbd49d2360d74f5e33660fc4f23ea2
LiviaTomaNeagu <livia.toma@student.unitbv.ro>
2025-02-01 12:52:06 +0200

    [Fix] Design fixes

--- package/src/app/layouts/full/sidebar/branding.component.ts
+++ package/src/app/layouts/full/sidebar/branding.component.ts
@@ -9,13 +9,24 @@ import { RouterModule } from '@angular/router';
     <div class="branding">
       <a [routerLink]="['/']">
         <img
-          src="./assets/images/logos/dark-logo.svg"
+          src="./assets/images/logos/main-logo.svg"
           class="align-middle m-2"
           alt="logo"
         />
       </a>
     </div>
-  `,
+  `,styles: [
+    `
+      .logo {
+        width: 10px !important; /* 🔥 Dimensiune fixă, ajustabilă */
+        height: auto; /* 🔥 Păstrează proporțiile */
+        max-width: 100%; /* 🔥 Se adaptează containerului */
+      }
+    
+    .branding {
+      padding: 20px 20px 0px 20px; 
+    }`,
+  ],
 })
 export class BrandingComponent {
   constructor() {}
